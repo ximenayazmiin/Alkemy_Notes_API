6e3154a5cad3267de676514eba82b26980e265c5
ximena <ximena.ih@xentra.mx>
2025-02-14 17:40:19 -0600

    Conf de .env

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 5000;
+  const url_nocors = process.env.URL_RESPUESTA_FRONT || 'http://localhost';
 
-  // Habilita CORS para permitir solicitudes desde http://localhost:3000
+  // Habilita CORS para permitir solicitudes desde http://localhost
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: url_nocors,
   });
 
-  await app.listen(3002);
+  await app.listen(port);
 }
 bootstrap();
